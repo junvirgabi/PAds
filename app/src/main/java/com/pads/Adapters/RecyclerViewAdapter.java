25d208e14ac4baee9f92618b7dd90816b4ecca0d
EarvinJay <earvinjaybermudo21@gmail.com>
2016-03-12 03:14:56 +0800

    Hoo> mao na gyud ni Adapter og views

---
+++ app/src/main/java/com/pads/Adapters/RecyclerViewAdapter.java
@@ -0,0 +1,86 @@
+package com.pads.Adapters;
+
+/**
+ * Created by srthg on 3/12/2016.
+ */
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.pads.Entities.Advertisements;
+import com.pads.R;
+import com.pads.Entities.Advertisements;
+
+import java.util.List;
+
+/**
+ * Created by srthg on 3/12/2016.
+ */
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MovieViewHolder> {
+
+    private Context mContext;
+    private int mLayoutId;
+    private List<Advertisements> mAds;
+
+    public RecyclerViewAdapter(Context context, int layoutId, List<Advertisements> ads) {
+        mContext = context;
+        mLayoutId = layoutId;
+        mAds = ads;
+    }
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(mLayoutId, parent, false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, int position) {
+        Advertisements ad = mAds.get(position);
+
+        if (ad != null) {
+//            if (holder.imgMovie != null) {
+//                holder.imgMovie.setImageResource(movie.getImageId());
+//            }
+            if (holder.tvName != null) {
+                holder.tvName.setText(ad.getAdsname());
+            }
+            if (holder.tvCategory != null) {
+                holder.tvCategory.setText(ad.getAdsCategory());
+            }
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mAds.size();
+    }
+
+    static class MovieViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public ImageView imgMovie;
+        public TextView tvName;
+        public TextView  tvCategory;
+
+        public MovieViewHolder(View view) {
+            super(view);
+
+            tvName = (TextView) view.findViewById(R.id.tvAdsName);
+            tvCategory = (TextView) view.findViewById(R.id.tvAdsCategory);
+
+            // once an item view (list item or grid item) is clicked
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/pads/Fragments/ListViewFragment.java
@@ -0,0 +1,86 @@
+package com.pads.Fragments;
+
+/**
+ * Created by srthg on 3/12/2016.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.pads.Adapters.AdvertisementsAdapter;
+import com.pads.Entities.Advertisements;
+import com.pads.R;
+import com.pads.Entities.Advertisements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by srthg on 3/12/2016.
+ */
+public class ListViewFragment extends Fragment implements AdapterView.OnItemClickListener {
+
+    private ListView mListView;
+    private TextView mTvEmpty;
+    private List<Advertisements> mAds= new ArrayList<>();
+
+
+    public static ListViewFragment newInstance() {
+        return new ListViewFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_listview, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        // find all the views
+        mListView = (ListView) view.findViewById(R.id.listView);
+        mTvEmpty = (TextView) view.findViewById(android.R.id.empty);
+
+        // create a new instance of adapter
+        AdvertisementsAdapter adapter = new AdvertisementsAdapter(getActivity(),R.layout.data_listview,mAds);
+
+        // set the adapter
+        mListView.setAdapter(adapter);
+
+        if (adapter.isEmpty()) {
+            mTvEmpty.setVisibility(View.VISIBLE);
+        } else {
+            mTvEmpty.setVisibility(View.GONE);
+        }
+
+        // set item click listener
+        mListView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+//    @Override
+//    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//        Intent intent = new Intent(getActivity(), MovieDetailsActivity.class);
+//        intent.putExtra("key_position", position);
+//        startActivity(intent);
+//    }
+}
+
